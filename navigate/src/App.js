498af576ff02dbe0d0f71dde3b316c17cd6982aa
lmason98 <lmason98@gmail.com>
2021-10-23 12:13:27 -0700

    Init project

---
+++ navigate/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+// react navigation app entry point
+import Body from './components/main/main.js';
+
+function App() {
+  return (
+    <Body />
+  );
+}
+
+export default App;
---
+++ navigate/src/components/main/main.js
@@ -0,0 +1,11 @@
+import './main.css';
+
+function Body() {
+  return (
+    <div className="main-div">
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
+
+export default Body;
