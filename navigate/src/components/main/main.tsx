9942a1e4ac352c92d40b7e0cb4969f4b47974fdb
lmason98 <lmason98@gmail.com>
2021-12-23 14:10:46 -0800

    add login page

--- navigate/src/components/main/main.tsx
+++ navigate/src/components/main/main.tsx
@@ -1,21 +1,29 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 
 import './main.css';
 
 import Body from '../body/body';
 import Footer from '../footer/footer';
 import Header from '../header/header';
+import Login from '../login/login';
 
 /*
 * Name: Main
 * Description: App entrypoint
 * */
 const Main: FunctionComponent = () => {
-  return (
-    <div className="main-div">
+  const [authed, setAuthed] = useState<Boolean>(false);
+
+  const content = authed ?
+    <>
       <Header />
       <Body />
       <Footer />
+    </> : <Login />;
+
+  return (
+    <div className="main-div">
+      {content}
     </div>
   );
 }
